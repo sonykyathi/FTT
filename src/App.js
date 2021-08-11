c2397290409f7662b41f64d0c2176239ad917220
AbdulRehman <ar.jameel096@gmail.com>
2021-08-11 16:04:54 +0500

    logo click show new in tab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import React, { Fragment } from "react";
 
@@ -7,20 +6,20 @@ import HomePage from "./components/home";
 import AboutusPage from "./components/pages/aboutus";
 import galleryPage from "./components/pages/gallery";
 import faqPage from "./components/pages/faq";
+import RoadMap from "./components/RoadMap";
 
 function App() {
   return (
-    <React.Fragment>
-      
+    <>
       <Router>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/aboutus' component={AboutusPage} />
-        <Route exact path='/gallery' component={galleryPage} />
-        <Route exact path='/faq' component={faqPage} />
-
-        
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/aboutus" component={AboutusPage} />
+          <Route exact path="/gallery" component={galleryPage} />
+          <Route exact path="/faq" component={faqPage} />
+        </Switch>
       </Router>
-    </React.Fragment>
+    </>
   );
 }
 
