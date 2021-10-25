e7f4772e726edb1d59da945767c02e08f14582ff
sonykyathi <sony@defitt.org>
2021-10-25 18:41:16 +0400

    bucket link changed

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,7 +6,7 @@ export const downloadWhitePaper = async () => {
     let res = await axios({
         method: 'get',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        url: `${process.env.REACT_APP_API}/api/v1/whitepaper`,
+        url: `${process.env.REACT_APP_API}/api/v1/whitepaper/ftt-whitepaper`,
     }).then(res => res.data);
 
     window.open(res.data, "_blank")
